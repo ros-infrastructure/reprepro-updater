5042219ee9ca6a71f2f04d4f383edaea988ef08c
Tully Foote <tfoote@willowgarage.com>
2013-06-14 15:21:26 -0700

    simplify sync command for fuerte and groovy as well

--- sync_groovy.sh
+++ sync_groovy.sh
@@ -8,14 +8,6 @@ wget http://packages.ros.org/ros-shadow-fixed/ubuntu/dists/precise/main/binary-a
 python ~/reprepro_updater/scripts/diff_packages.py old_packages new_packages groovy > logs/groovy_sync_`date +%Y-%m-%d-%T`
 
 
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -r groovy -d oneiric -a i386 -u file:/var/packages/ros-shadow-fixed/ubuntu -c
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -r groovy -d oneiric -a amd64 -u file:/var/packages/ros-shadow-fixed/ubuntu -c
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -r groovy -d oneiric -a source -u file:/var/packages/ros-shadow-fixed/ubuntu -c
-
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -r groovy -d precise -a i386 -u file:/var/packages/ros-shadow-fixed/ubuntu -c
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -r groovy -d precise -a amd64 -u file:/var/packages/ros-shadow-fixed/ubuntu -c
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -r groovy -d precise -a source -u file:/var/packages/ros-shadow-fixed/ubuntu -c
-
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -r groovy -d quantal -a i386 -u file:/var/packages/ros-shadow-fixed/ubuntu -c
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -r groovy -d quantal -a amd64 -u file:/var/packages/ros-shadow-fixed/ubuntu -c
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -r groovy -d quantal -a source -u file:/var/packages/ros-shadow-fixed/ubuntu -c
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -r groovy -d oneiric -d precise -d quantal -a i386 -u file:/var/packages/ros-shadow-fixed/ubuntu -c
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -r groovy -d oneiric -d precise -d quantal -a amd64 -u file:/var/packages/ros-shadow-fixed/ubuntu -c
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -r groovy -d oneiric -d precise -d quantal -a source -u file:/var/packages/ros-shadow-fixed/ubuntu -c
