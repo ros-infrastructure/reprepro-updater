b6077c87822c410aa2c4dcebc691b600befcd09d
Tully Foote <tfoote@willowgarage.com>
2013-07-05 21:02:45 -0400

    adding gazebo to upstream imports

--- update_pub8_external.sh
+++ update_pub8_external.sh
@@ -13,3 +13,6 @@ python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros
 
 python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/config/bullet.upstream.yaml -c
 python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -y /home/rosbuild/reprepro_updater/config/bullet.upstream.yaml -c
+
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/config/gazebo.upstream.yaml -c
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -y /home/rosbuild/reprepro_updater/config/gazebo.upstream.yaml -c
