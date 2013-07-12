c3f67cf25b9b5693a6bc09292b7c2f0663999133
Tully Foote <tfoote@willowgarage.com>
2013-07-12 14:05:14 -0700

    doing batch uploading of multiple folders to support https://github.com/ros-infrastructure/buildfarm/issues/115

--- scripts/include_folder.py
+++ scripts/include_folder.py
@@ -2,22 +2,28 @@ from optparse import OptionParser
 
 import os
 import sys
-import shutil 
+import shutil
 
-from reprepro_updater.helpers import try_run_command, LockContext, delete_unreferenced
+from reprepro_updater.helpers import \
+    LockContext, delete_unreferenced,\
+    run_update_command, invalidate_dependent, invalidate_package
+from reprepro_updater.changes_parsing import \
+    find_changes_files, load_changes_files
 
-ALL_DISTROS = ['hardy', 'jaunty', 'karmic', 'lucid', 'maverick', 'natty', 'oneiric', 'precise', 'quantal', 'raring', 'wheezy']
+ALL_DISTROS = ['hardy', 'jaunty', 'karmic', 'lucid',
+               'maverick', 'natty', 'oneiric',
+               'precise', 'quantal', 'raring', 'wheezy']
 
-ALL_ARCHES =  ['amd64', 'i386', 'armel', 'armhf', 'source']
+ALL_ARCHES = ['amd64', 'i386', 'armel', 'armhf', 'source']
 
 parser = OptionParser()
 
 parser.add_option("--delete-folder", dest="do_delete", action='store_true', default=False)
 
-parser.add_option("-d", "--distro", dest="distro")
-parser.add_option("-a", "--arch", dest="arch")
+parser.add_option("-d", "--distro", dest="distro", help="unused")
+parser.add_option("-a", "--arch", dest="arch", help="unused")
 
-parser.add_option("-f", "--folder", dest="folder")
+parser.add_option("-f", "--folder", dest="folders", action="append")
 parser.add_option("-p", "--package", dest="package")
 
 parser.add_option("-c", "--commit", dest="commit", action='store_true', default=False)
@@ -28,77 +34,62 @@ parser.add_option("--repo-path", dest="repo_path", default='/var/www/repos/build
 
 (options, args) = parser.parse_args()
 
-if not options.distro:
-    parser.error("distro required")
 
-if not options.distro in ALL_DISTROS:
-    parser.error("invalid distro %s, not in %s" % (options.distro, ALL_DISTROS))
+for f in options.folders:
+    if not os.path.isdir(f):
+        parser.error("Folder option must be a folder: %s" % f)
 
-if not options.arch:
-    parser.error("arch required")
+changefiles = load_changes_files(find_changes_files(options.folders))
 
-if not options.arch in ALL_ARCHES:
-    parser.error("invalid arch %s, not in %s" % (options.arch, ALL_ARCHES))
+if not changefiles:
+    parser.error("Folders %s doesn't contain a changes file. %s" %
+                 (options.folders, [os.listdir(f) for f in options.folders]))
 
+valid_changes = [c for c in changefiles if c.package == options.package]
 
-if not os.path.isdir(options.folder):
-    parser.error("Folder option must be a folder: %s" % options.folder)
-
-    #cleanup_command = ['reprepro', '-v', '-b', options.repo_path, '-A', options.arch, 'removefilter', options.distro, "Package (%% ros-%s-* )"% options.rosdistro]
-
-changesfile = None
-
-for f in os.listdir(options.folder):
-    if f.endswith('.changes'):
-        changesfile = os.path.join(options.folder, f)
-        break
-
-if not changesfile:
-    parser.error("Folder %s doesn't contain a changes file. %s" % (options.folder, os.listdir(options.folder)))
-
-# Force misc due to dry packages having invalid "unknown" section, the -S misc can be removed when dry is deprecated. 
-update_command = ['reprepro', '-v', '-b', options.repo_path, '-S', 'misc', 'include', options.distro, changesfile]
-
-
-debtype = 'deb' if options.arch != 'source' else 'dsc'
-arch_match = ', Architecture (== ' + options.arch + ' )' if options.arch != 'source' else ''
-
-invalidate_dependent_command = ['reprepro', '-b', options.repo_path, '-T', debtype, '-V', 'removefilter', options.distro,
-                              "Package (% ros-* )"+arch_match+", ( Depends (% *"+options.package+"[, ]* ) | Depends (% *"+options.package+" ) )"]
-
-invalidate_package_command = ['reprepro', '-b', options.repo_path, '-T', debtype, '-V', 'removefilter', options.distro,
-                              "Package (== "+options.package+" )"+arch_match]
-
-
+extraneous_packages = set(changefiles) - set(valid_changes)
+if extraneous_packages:
+    parser.error("Invalid packages detected in folders %s. [%s]" %
+                 (options.folders, extraneous_packages))
 
 lockfile = os.path.join(options.repo_path, 'lock')
-
-
 
 if options.commit:
     with LockContext(lockfile) as lock_c:
 
-        if options.invalidate:
-            if options.arch != 'source':
-                print >>sys.stderr, "running", invalidate_dependent_command
-                if not try_run_command(invalidate_dependent_command):
-                    sys.exit(1)
+        #invalidate and clear all first
 
-            print >>sys.stderr, "running", invalidate_package_command
-            if not try_run_command(invalidate_package_command):
-                sys.exit(1)
-                
-            if not delete_unreferenced(options.repo_path):
-                sys.exit(1)
+        for changes in valid_changes:
+            # only invalidate dependencies if invalidation is asked for
+            if options.invalidate:
 
+                if changes.arch != 'source':
+                    if not invalidate_dependent(options.repo_path,
+                                                changes.distro,
+                                                changes.architecture,
+                                                options.package):
+                        sys.exit(1)
 
-        print >>sys.stderr, "running command %s" % update_command
-
-        if not try_run_command(update_command):
+        # invalidate this package always as we're about to upload the new one
+        if not invalidate_package(options.repo_path,
+                                  changes.distro,
+                                  changes.architecture,
+                                  options.package):
             sys.exit(1)
-        if options.do_delete:
-            print "Removing %s" % options.folder
-            shutil.rmtree(options.folder)
+        # delete_unreferenced before uploading if invalidating
+        if not delete_unreferenced(options.repo_path):
+            sys.exit(1)
+
+        # update after clearing all
+        for changes in valid_changes:
+
+            if not run_update_command(options.repo_path,
+                                      changes.distro,
+                                      changes.filename):
+                sys.exit(1)
+            if options.do_delete:
+                print "Removing %s" % changes.folder
+                shutil.rmtree(changes.folder)
 
 else:
-    print >>sys.stderr, "NO COMMIT OPTION\nWould have run: ", invalidate_dependent_command, "\nThen ", invalidate_package_command, "\nThen ", update_command
+    print >>sys.stderr, "NO COMMIT OPTION\nWould have run invalidation of dependent packages, invalidation of %s package and uploaded new package" % (options.package)
---
+++ src/reprepro_updater/changes_parsing.py
@@ -0,0 +1,31 @@
+import os
+import yaml
+
+
+class ChangesFile:
+    def __init__(self, filename):
+        try:
+            self.filename = filename
+            self.yaml_content = yaml.load(open(filename).read())
+            self.architecture = self.yaml_content['Architecture']
+            self.distro = self.yaml_content['Distribution']
+            self.package = self.yaml_content['Binary']
+            self.folder = os.path.dirname(filename)
+        except Exception, ex:
+            raise Exception("Failed to load changes file %s.  [[%s]]" %
+                            (filename, ex.what()))
+
+
+def find_changes_files(folder):
+    changesfiles = []
+    for f in os.listdir(folder):
+        if f.endswith('.changes'):
+            changesfiles.append(os.path.join(folder, f))
+    return changesfiles
+
+
+def load_changes_files(changes_files):
+    changesfile_objs = []
+    for f in changes_files:
+        changesfile_objs.append(ChangesFile(f))
+    return changesfile_objs
