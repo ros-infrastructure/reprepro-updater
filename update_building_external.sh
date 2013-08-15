79cc2705c965d230769700fb50e664f23ad4b5a6
Tully Foote <tfoote@willowgarage.com>
2013-08-15 15:57:40 -0400

    renaming openni.upstream.yaml to pcl.upstream.yaml, updating references.

--- update_building_external.sh
+++ update_building_external.sh
@@ -2,7 +2,7 @@
 
 export PYTHONPATH=/home/rosbuild/reprepro_updater/src
 
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/building -y /home/rosbuild/reprepro_updater/config/openni.upstream.yaml -c
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/building -y /home/rosbuild/reprepro_updater/config/pcl.upstream.yaml -c
 
 python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/building -y /home/rosbuild/reprepro_updater/config/colladadom.upstream.yaml -c
 python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/building -y /home/rosbuild/reprepro_updater/config/bullet.upstream.yaml -c
--- update_pub8_external.sh
+++ update_pub8_external.sh
@@ -2,9 +2,9 @@
 
 export PYTHONPATH=/home/rosbuild/reprepro_updater/src
 
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/config/openni.upstream.yaml -c
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/config/pcl.upstream.yaml -c
 
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -y /home/rosbuild/reprepro_updater/config/openni.upstream.yaml -c
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -y /home/rosbuild/reprepro_updater/config/pcl.upstream.yaml -c
 
 
 python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/config/colladadom.upstream.yaml -c
