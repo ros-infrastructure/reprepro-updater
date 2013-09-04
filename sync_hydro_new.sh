02d9eafd99f94460b59cd00c878b76ebda7f71a6
Tully Foote <tfoote@willowgarage.com>
2013-09-04 18:30:03 -0400

    updating new sync script to actually run

--- sync_hydro_new.sh
+++ sync_hydro_new.sh
@@ -8,6 +8,6 @@ python ~/reprepro_updater/scripts/diff_packages.py /var/www/repos/ros/ubuntu/dis
 
 
 
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -r hydro -d precise -d quantal -d raring -a i386 -u file:/var/www/repos/ros-shadow-fixed/ubuntu 
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -r hydro -d precise -d quantal -d raring -a amd64 -u file:/var/www/repos/ros-shadow-fixed/ubuntu
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -r hydro -d precise -d quantal -d raring -a source -u file:/var/www/repos/ros-shadow-fixed/ubuntu
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -r hydro -d precise -d quantal -d raring -a i386 -u file:/var/www/repos/ros-shadow-fixed/ubuntu -c
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -r hydro -d precise -d quantal -d raring -a amd64 -u file:/var/www/repos/ros-shadow-fixed/ubuntu -c
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -r hydro -d precise -d quantal -d raring -a source -u file:/var/www/repos/ros-shadow-fixed/ubuntu -c
