a6e1fdd99f912dd5450aaeee66d4c6df0aaadb6b
Tully Foote <tfoote@willowgarage.com>
2013-10-10 13:32:14 -0400

    adding commit to the commands in groovy sync

--- sync_groovy.sh
+++ sync_groovy.sh
@@ -6,6 +6,6 @@ export PYTHONPATH=/home/rosbuild/reprepro_updater/src
 python ~/reprepro_updater/scripts/diff_packages.py /var/www/repos/ros/ubuntu/dists/precise/main/binary-amd64/Packages /var/www/repos/ros-shadow-fixed/ubuntu/dists/precise/main/binary-amd64/Packages groovy > logs/groovy_sync_`date +%Y-%m-%d-%T`
 
 
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -r groovy -d oneiric -d precise -d quantal -a i386 -u file:/var/www/repos/ros-shadow-fixed/ubuntu
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -r groovy -d oneiric -d precise -d quantal -a amd64 -u file:/var/www/repos/ros-shadow-fixed/ubuntu
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -r groovy -d oneiric -d precise -d quantal -a source -u file:/var/www/repos/ros-shadow-fixed/ubuntu
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -r groovy -d oneiric -d precise -d quantal -a i386 -u file:/var/www/repos/ros-shadow-fixed/ubuntu -c
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -r groovy -d oneiric -d precise -d quantal -a amd64 -u file:/var/www/repos/ros-shadow-fixed/ubuntu -c
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -r groovy -d oneiric -d precise -d quantal -a source -u file:/var/www/repos/ros-shadow-fixed/ubuntu -c
