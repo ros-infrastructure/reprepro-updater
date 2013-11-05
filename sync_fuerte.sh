43267cc49bb5707b935dae36cfe72ddab4e8c54a
Tully Foote <tfoote@willowgarage.com>
2013-11-05 01:44:44 -0500

    updating sync scripts for the new diff_package.py usage

--- sync_fuerte.sh
+++ sync_fuerte.sh
@@ -3,7 +3,7 @@
 export PYTHONPATH=/home/rosbuild/reprepro_updater/src
 
 # export dump of change
-python ~/reprepro_updater/scripts/diff_packages.py /var/www/repos/ros/ubuntu/dists/precise/main/binary-amd64/Packages /var/www/repos/ros-shadow-fixed/ubuntu/dists/precise/main/binary-amd64/Packages fuerte > logs/fuerte_sync_`date +%Y-%m-%d-%T`
+python ~/reprepro_updater/scripts/diff_packages.py /var/www/repos/ros/ubuntu/dists/precise/main/binary-amd64/Packages /var/www/repos/ros-shadow-fixed/ubuntu/dists/precise/main/binary-amd64/Packages fuerte --output-dir ~/reprepro_updater/logs/
 
 
 python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -r groovy -d lucid -d oneiric -d precise -a i386 -u file:/var/www/repos/ros-shadow-fixed/ubuntu
--- sync_hydro.sh
+++ sync_hydro.sh
@@ -3,7 +3,7 @@
 export PYTHONPATH=/home/rosbuild/reprepro_updater/src
 
 # export dump of change
-python ~/reprepro_updater/scripts/diff_packages.py /var/www/repos/ros/ubuntu/dists/precise/main/binary-amd64/Packages /var/www/repos/ros-shadow-fixed/ubuntu/dists/precise/main/binary-amd64/Packages hydro > logs/hydro_sync_`date +%Y-%m-%d-%T`
+python ~/reprepro_updater/scripts/diff_packages.py /var/www/repos/ros/ubuntu/dists/precise/main/binary-amd64/Packages /var/www/repos/ros-shadow-fixed/ubuntu/dists/precise/main/binary-amd64/Packages hydro --output-dir ~/reprepro_updater/logs
 
 # i386
 python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -r hydro -d precise -d quantal -d raring -a i386 -u file:/var/www/repos/ros-shadow-fixed/ubuntu -c
