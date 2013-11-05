dc4d28e0f3796cb6706d5e7eaed330fd2d123d8a
Tully Foote <tfoote@willowgarage.com>
2013-11-05 01:22:13 -0500

    updating diff_packages to be much cleaner and print maintainers too

--- scripts/diff_packages.py
+++ scripts/diff_packages.py
@@ -1,10 +1,43 @@
-import difflib
 import argparse
+import re
+import datetime
+import os
+
+from debian.debian_support import PackageFile
 
 
+def convert_tuples_list_to_dict(tuples_list):
+    output = {}
+    for (one, two) in tuples_list:
+        output[one] = two
+    return output
+
+def strip_email(maintainer):
+    return re.sub("(.*)<.*>", "\\1", maintainer)
+
+def core_version(version):
+    return core_debian_version(core_rosbuild_version(version))
+
+def core_debian_version(version): # TODO remove hard coded ubuntu versions here
+    return re.sub("(.*)(precise|quantal)-\d{8}-\d{4}-\+\d{4}",
+                  "\\1", version)
+
+def core_rosbuild_version(version):
+    return re.sub("(.*)-s\d{10}~\w*",
+                  "\\1",
+                  version)
+
+def is_substantial_version_change(v1, v2):
+    cv1 = core_version(v1)
+    cv2 = core_version(v2)
+    #print("core %s %s" % (cv1, cv2))
+
+    return cv1 != cv2
+
+    
 def main():
     """
-    Usage: python syncdiff.py beforefile afterfile
+    Usage: python diff_packages.py beforefile afterfile [--output-dir dir]
     Output: list of added/removed/versioned packages
     """
     usage = "usage: %prog fromfile tofile [rosdistro]"
@@ -12,92 +45,94 @@ def main():
     parser.add_argument('fromfile', type=str, nargs='+', default=None)
     parser.add_argument('tofile', type=str, nargs='+', default=None)
     parser.add_argument('rosdistro', type=str, default='groovy')
+    parser.add_argument('--output-dir', dest='output_dir', type=str, default='.')
 
     args = parser.parse_args()
 
+    if not os.path.isdir(args.output_dir):
+        parser.error("Output directory [%s] does not exist aborting." % args.output_dir)
+        
+    if not os.path.exists(args.fromfile[0]):
+        parser.error("Missing input file from %s" % args.fromfile[0])
+
+    if not os.path.exists(args.tofile[0]):
+        parser.error("Missing input file from %s" % args.tofile[0])
+
+
     files = [args.fromfile[0], args.tofile[0]]
-    fromlines = open(files[0], 'U').readlines()
-    tolines = open(files[1], 'U').readlines()
-    diff = difflib.unified_diff(fromlines, tolines, files[0], files[1], n=5)
-    diff = [l for l in diff if ('Version:' == l[1:9] or 'Package:' == l[1:9])]
+    fromlines = open(files[0], 'U')
+    tolines = open(files[1], 'U')
+    pf_old = PackageFile('old', fromlines)
+    pf_new = PackageFile('new', tolines)
 
-    added_packages = {}
-    removed_packages = {}
-    versioned_packages = {}
+    old_packages = {}
+    new_packages = {}
 
-    current_package = None
-    current_package_mod = None
-    current_version = None
-    current_version_mod = None
-    for line in diff:
-        try:
-            if 'Package:' == line[1:9]:
-                current_package = line[line.index(':') + 2:-1]
-                current_package_mod = line[0]
-            elif 'Version:' == line[1:9]:
-                current_version = version(line[line.index(':') + 2:-1])
-                current_version_mod = line[0]
-                if current_package_mod is ' ':
-                    if current_package not in versioned_packages:
-                        versioned_packages[current_package] = [None, None]
-                    if current_version_mod == '-':
-                        versioned_packages[current_package][0] = current_version
-                    elif current_version_mod == '+':
-                        versioned_packages[current_package][1] = current_version
-                    else:
-                        pass  # Diff static, Version not changed, ignore
-                elif current_package_mod is '+':
-                    if current_version_mod is not '+':
-                        raise Exception(current_package + " add package odd ver")
-                    if current_package in removed_packages:
-                        temp = [current_version, removed_packages[current_package]]
-                        del removed_packages[current_package]
-                        versioned_packages[current_package] = temp
-                        versioned_packages[current_package].sort()
-                    else:
-                        added_packages[current_package] = current_version
-                elif current_package_mod is '-':
-                    if current_version_mod is not '-':
-                        raise Exception(current_package + " rem package odd ver")
-                    if current_package in added_packages:
-                        temp = [current_version, added_packages[current_package]]
-                        del added_packages[current_package]
-                        versioned_packages[current_package] = temp
-                        versioned_packages[current_package].sort()
-                    else:
-                        removed_packages[current_package] = current_version
-                else:
-                    raise Exception("Erronious character: " + current_package_mod)
-            else:
-                raise Exception("Erronious line found: " + line)
-        except Exception as ex:
-            print "Excption parsing line:", ex
-
-    print "\nPackages Added: "
-    for line in sorted(added_packages):
-        if args.rosdistro not in line:
+    for p in pf_old:
+        intermediate = convert_tuples_list_to_dict(p)
+        name = intermediate['Package']
+        if args.rosdistro not in name:
             continue
-        print line, ':', added_packages[line]
-    print "\nPackages Removed: "
-    for line in sorted(removed_packages):
-        if args.rosdistro not in line:
-            continue
-        print line, ':', removed_packages[line]
-    print "\nPackages Updated: "
-    for line in sorted(versioned_packages):
-        if args.rosdistro not in line:
-            continue
-        print_package = versioned_packages[line]
-        if len(print_package) == 2 and print_package[0] != print_package[1]:
-            print line, ':', print_package[0], '->', print_package[1]
+        old_packages[name] = intermediate
 
 
-def version(full_string):
-    sectioned = full_string.split('-', 2)
-    version_number = sectioned[0]
-    if sectioned[1].isdigit():
-        version_number = version_number + '-' + sectioned[1]
-    return version_number
+    for p in pf_new:
+        intermediate = convert_tuples_list_to_dict(p)
+        name = intermediate['Package']
+        if args.rosdistro not in name:
+            continue
+        new_packages[intermediate['Package']] = intermediate
+
+    updated_packages = set()
+    removed_packages = set()
+    added_packages = set()
+
+    for p in [p for p in new_packages if p in old_packages]:
+        if new_packages[p]['Version'] == old_packages[p]['Version']:
+            continue
+        if is_substantial_version_change(new_packages[p]['Version'], old_packages[p]['Version']):
+            updated_packages.add(p)
+
+    added_packages = set([p for p in new_packages if p not in old_packages])
+    removed_packages = set([p for p in old_packages if p not in new_packages])
+
+    maintainers = set()
+    for p in added_packages | updated_packages:
+        maintainers.add(strip_email(new_packages[p]['Maintainer']).strip())
+
+
+    dtime = datetime.datetime.now()
+    dtime = dtime.replace(microsecond = 0)
+    filename = os.path.join(args.output_dir, '%s_%s.txt' % (args.rosdistro, dtime.isoformat('-')))
+
+    print("Writing to file %s" % filename)
+    with open(filename, 'w') as fh:
+        fh.write("Updates to %s\n\n" % args.rosdistro)
+        fh.write("Added Packages\n")
+        for p in added_packages:
+            fh.write("- %s: %s\n" % (p, core_version(new_packages[p]['Version'])))
+        fh.write("\n\n")
+
+        fh.write("Updated Packages:\n")
+        for p in updated_packages:
+            fh.write("- %s: %s -> %s\n" % (p, 
+                                            core_version(old_packages[p]['Version']),
+                                            core_version(new_packages[p]['Version'])))
+        fh.write("\n\n")
+
+        fh.write("Removed Packages:\n")
+        for p in removed_packages:
+            fh.write("- %s\n" % (p, core_version(old_packages[p]['Version'])))
+        fh.write("\n\n")
+
+        fh.write("Thanks to all ROS maintainers who make packages"
+                 " available to the ROS community. The above list "
+                 "of packages was made possible by the work of the"
+                 " following maintainers:\n")
+        for maintainer in sorted(maintainers):
+            fh.write(" - %s\n"  % maintainer)
+
+    
 
 if __name__ == '__main__':
     main()
