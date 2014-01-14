f87b96003650a42bfb1938ae530b26e134fe80ba
Tully Foote <tfoote@willowgarage.com>
2014-01-14 18:55:52 -0500

    update script now for repos.ros.org

--- update_pub8_external.sh
+++ update_pub8_external.sh
@@ -2,17 +2,17 @@
 
 export PYTHONPATH=/home/rosbuild/reprepro_updater/src
 
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/config/pcl.upstream.yaml -c
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/config/pcl.upstream.yaml -c
 
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -y /home/rosbuild/reprepro_updater/config/pcl.upstream.yaml -c
-
-
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/config/colladadom.upstream.yaml -c
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -y /home/rosbuild/reprepro_updater/config/colladadom.upstream.yaml -c
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -y /home/rosbuild/reprepro_updater/config/pcl.upstream.yaml -c
 
 
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/config/bullet.upstream.yaml -c
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -y /home/rosbuild/reprepro_updater/config/bullet.upstream.yaml -c
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/config/colladadom.upstream.yaml -c
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -y /home/rosbuild/reprepro_updater/config/colladadom.upstream.yaml -c
 
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/config/gazebo.upstream.yaml -c
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros/ubuntu -y /home/rosbuild/reprepro_updater/config/gazebo.upstream.yaml -c
+
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/config/bullet.upstream.yaml -c
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -y /home/rosbuild/reprepro_updater/config/bullet.upstream.yaml -c
+
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/config/gazebo.upstream.yaml -c
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -y /home/rosbuild/reprepro_updater/config/gazebo.upstream.yaml -c
