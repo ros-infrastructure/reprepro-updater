a267e1edce344303af1ca3da90771ee02abb18b4
Tully Foote <tfoote@osrfoundation.org>
2014-10-07 16:58:28 -0700

    remove hardcoded key fingerprint

--- scripts/prepare_sync.py
+++ scripts/prepare_sync.py
@@ -60,6 +60,7 @@ parser = OptionParser()
 parser.add_option("-r", "--rosdistro", dest="rosdistro")
 parser.add_option("-a", "--arch", dest="arch")
 parser.add_option("-d", "--distro", dest="distro", action='append', default=[])
+parser.add_option("-k", "--sign-key", dest="key", default=None)
 parser.add_option("-u", "--upstream-ros", dest="upstream_ros", default=None)
 parser.add_option("-y", "--yaml-upstream", dest="yaml_upstream", default=[], action='append')
 parser.add_option("-n", "--no-cleanup", dest="no_cleanup", default=False, action='store_true')
@@ -108,7 +109,7 @@ if not os.path.isdir(conf_dir):
 updates_generator = conf.UpdatesFile([options.rosdistro], ALL_DISTROS, ALL_ARCHES)
 update_filename = os.path.join(conf_dir, 'updates')
 
-dist = conf.DistributionsFile(ALL_DISTROS, ALL_ARCHES, 'B01FA116' , updates_generator)
+dist = conf.DistributionsFile(ALL_DISTROS, ALL_ARCHES, options.key , updates_generator)
 distributions_filename = os.path.join(conf_dir, 'distributions')
 
 
