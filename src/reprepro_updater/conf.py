2398d9e5b2e2dec084e31d5a390284daba66ca7a
Tully Foote <tfoote@willowgarage.com>
2014-10-16 02:55:52 -0400

    adding utopic re: https://github.com/ros/rosdistro/issues/5944#issuecomment-59141433

--- src/reprepro_updater/conf.py
+++ src/reprepro_updater/conf.py
@@ -35,7 +35,7 @@ import os
 
 ALL_DISTROS = ['hardy', 'jaunty', 'karmic', 'lucid', 'maverick',
                'natty', 'oneiric', 'precise', 'quantal', 'raring',
-               'saucy', 'trusty',
+               'saucy', 'trusty', 'utopic',
                'wheezy', 'jessie']
 ALL_ARCHES = ['amd64', 'i386', 'armel', 'armhf', 'source']
 
