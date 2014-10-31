f0ee1c947ad78eabe0acc19a3fddf089abb54b57
Tully Foote <tfoote@osrfoundation.org>
2014-10-31 11:43:30 -0700

    use new syntax for import_upstream

--- update_building_external.sh
+++ update_building_external.sh
@@ -2,13 +2,4 @@
 
 export PYTHONPATH=/home/rosbuild/reprepro_updater/src
 
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/building -y /home/rosbuild/reprepro_updater/config/pcl.upstream.yaml -c
-
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/building -y /home/rosbuild/reprepro_updater/config/colladadom.upstream.yaml -c
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/building -y /home/rosbuild/reprepro_updater/config/bullet.upstream.yaml -c
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/building -y /home/rosbuild/reprepro_updater/config/gazebo.upstream.yaml -c
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/building -y /home/rosbuild/reprepro_updater/config/gazebo2.upstream.yaml -c
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/building/ -y /home/rosbuild/reprepro_updater/config/qtsixa.upstream.yaml -c
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/building/ -y /home/rosbuild/reprepro_updater/config/catkin_lint.upstream.yaml -c
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/building/ -y /home/rosbuild/reprepro_updater/config/ceres.upstream.yaml -c
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/building/ -y /home/rosbuild/reprepro_updater/config/urdfdom.upstream.yaml -c
+python /home/rosbuild/reprepro_updater/scripts/import_upstream.py /var/www/repos/building /home/rosbuild/reprepro_updater/config/pcl.upstream.yaml  /home/rosbuild/reprepro_updater/config/colladadom.upstream.yaml  /home/rosbuild/reprepro_updater/config/bullet.upstream.yaml  /home/rosbuild/reprepro_updater/config/gazebo.upstream.yaml  /home/rosbuild/reprepro_updater/config/gazebo2.upstream.yaml  /home/rosbuild/reprepro_updater/config/qtsixa.upstream.yaml /home/rosbuild/reprepro_updater/config/catkin_lint.upstream.yaml /home/rosbuild/reprepro_updater/config/ceres.upstream.yaml /home/rosbuild/reprepro_updater/config/urdfdom.upstream.yaml -c
