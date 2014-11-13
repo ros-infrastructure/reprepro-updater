f4a617dc6a37f6f1dfceba3cff38daca38d27a1d
Tully Foote <tfoote@osrfoundation.org>
2014-11-12 21:51:56 -0800

    move test script to test and clear a application specific script

--- scripts/update_repos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script will run setup_repos.py on the locations where ros repos are expected on repos.ros.org
-# This is also useful when adding support for a new Ubuntu distro. 
-
-export PYTHONPATH=/home/rosbuild/reprepro_updater/src
-python /home/rosbuild/reprepro_updater/scripts/setup_repo.py /var/www/repos/building -c
-python /home/rosbuild/reprepro_updater/scripts/setup_repo.py /var/www/repos/ros-shadow-fixed/ubuntu -c
-python /home/rosbuild/reprepro_updater/scripts/setup_repo.py /var/www/repos/ros/ubuntu -c
