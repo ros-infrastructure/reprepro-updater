7c17c31e115029990a8f2e103db57f40c215e7e7
Tully Foote <tfoote@osrfoundation.org>
2014-12-01 13:20:47 -0800

    clean up sync script, leverage new config options

--- scripts/sync_ros_packages.py
+++ scripts/sync_ros_packages.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python
+
 from reprepro_updater import conf
-from reprepro_updater.conf import ALL_ARCHES, ALL_DISTROS
 from reprepro_updater.helpers import LockContext, run_cleanup, run_update
 
 from optparse import OptionParser
@@ -14,8 +15,8 @@ parser = OptionParser()
 parser.add_option("-r", "--rosdistro", dest="rosdistro")
 parser.add_option("-a", "--arch", dest="arch")
 parser.add_option("-d", "--distro", dest="distro", action='append', default=[])
-parser.add_option("-k", "--sign-key", dest="key", default=None)
-parser.add_option("-u", "--upstream-ros", dest="upstream_ros", default=None)
+parser.add_option("-u", "--upstream-ros", dest="upstream_ros", default=None,
+                  help="The upstream repository url to pull from")
 parser.add_option("-n", "--no-cleanup", dest="no_cleanup",
                   default=False, action='store_true')
 parser.add_option("-c", "--commit", dest="commit",
@@ -26,42 +27,33 @@ parser.add_option("-c", "--commit", dest="commit",
 if not len(args) == 1:
     parser.error("must be just one arguments, the directory to write into")
 
+conf_params = conf.load_conf(args[0])
+if not conf_params.repo_exists():
+    parser.error("Repository must have been initialized already")
+
 if not options.distro:
     parser.error("distro required")
 
 if not options.upstream_ros:
     parser.error("upstream-ros required")
 
-if not [d for d in options.distro if d in ALL_DISTROS]:
-    parser.error("invalid distros %s, not in %s" %
-                 (options.distro, ALL_DISTROS))
+invalid_distros = [d for d in options.distro if d not in conf_params.distros]
+if invalid_distros:
+    parser.error("invalid distros %s, not in repo configuration: %s" %
+                 (invalid_distros, conf_params.distros))
 
 if not options.rosdistro:
     parser.error("rosdistro required")
 
-if not options.arch:
-    parser.error("arch required")
+if options.arch not in conf_params.architectures:
+    parser.error("invalid arch %s, not in repo configuration: %s" %
+                 (options.arch,
+                  conf_params.architectures))
 
-if options.arch not in ALL_ARCHES:
-    parser.error("invalid arch %s, not in %s" % (options.arch, ALL_ARCHES))
+updates_generator = conf.UpdatesFile(conf_params.distros,
+                                     conf_params.architectures)
 
-repo_dir = args[0]
-conf_dir = os.path.join(repo_dir, 'conf')
-
-if not os.path.isdir(conf_dir):
-    parser.error("Argument must be an existing reprepro")
-
-updates_generator = conf.UpdatesFile([options.rosdistro],
-                                     ALL_DISTROS, ALL_ARCHES)
-update_filename = os.path.join(conf_dir, 'updates')
-
-dist = conf.DistributionsFile(ALL_DISTROS, ALL_ARCHES,
-                              options.key, updates_generator)
-distributions_filename = os.path.join(conf_dir, 'distributions')
-
-
-target_arches = set()
-target_distros = set()
+dist = conf_params.create_distributions_file(updates_generator)
 
 for ubuntu_distro in options.distro:
 
@@ -79,8 +71,8 @@ for ubuntu_distro in options.distro:
 # clean up first
 if not options.no_cleanup:
     for distro in options.distro:
-        run_cleanup(repo_dir, options.rosdistro, distro,
+        run_cleanup(conf_params.repository_path, options.rosdistro, distro,
                     options.arch, options.commit)
 for distro in options.distro:
-    run_update(repo_dir, dist, updates_generator,
+    run_update(conf_params.repository_path, dist, updates_generator,
                distro, options.arch, options.commit)
