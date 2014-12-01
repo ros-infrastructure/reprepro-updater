2362ee146798791afcbf820c0e369e604ce2f5ac
Tully Foote <tfoote@osrfoundation.org>
2014-12-01 13:36:55 -0800

    add support for multiple arches in a single command. And make the distros and arches default to all the distros and arches in the configured repo

--- scripts/sync_ros_packages.py
+++ scripts/sync_ros_packages.py
@@ -13,8 +13,10 @@ import yaml
 
 parser = OptionParser()
 parser.add_option("-r", "--rosdistro", dest="rosdistro")
-parser.add_option("-a", "--arch", dest="arch")
-parser.add_option("-d", "--distro", dest="distro", action='append', default=[])
+parser.add_option("-a", "--arch", dest="arches", action='append',
+                  default=[], help='Override repo configuration arches')
+parser.add_option("-d", "--distro", dest="distros", action='append',
+                  default=[], help='Override repo configuration distros')
 parser.add_option("-u", "--upstream-ros", dest="upstream_ros", default=None,
                   help="The upstream repository url to pull from")
 parser.add_option("-n", "--no-cleanup", dest="no_cleanup",
@@ -31,13 +33,10 @@ conf_params = conf.load_conf(args[0])
 if not conf_params.repo_exists():
     parser.error("Repository must have been initialized already")
 
-if not options.distro:
-    parser.error("distro required")
-
 if not options.upstream_ros:
     parser.error("upstream-ros required")
 
-invalid_distros = [d for d in options.distro if d not in conf_params.distros]
+invalid_distros = [d for d in options.distros if d not in conf_params.distros]
 if invalid_distros:
     parser.error("invalid distros %s, not in repo configuration: %s" %
                  (invalid_distros, conf_params.distros))
@@ -45,34 +44,50 @@ if invalid_distros:
 if not options.rosdistro:
     parser.error("rosdistro required")
 
-if options.arch not in conf_params.architectures:
-    parser.error("invalid arch %s, not in repo configuration: %s" %
-                 (options.arch,
+invalid_arches = [a for a in options.arches
+                  if a not in conf_params.architectures]
+if invalid_arches:
+    parser.error("invalid arches %s, not in repo configuration: %s" %
+                 (options.arches,
                   conf_params.architectures))
+
+if options.arches:
+    arches = options.arches
+else:
+    arches = conf_params.architectures
+
+if options.distros:
+    distros = options.distros
+else:
+    distros = conf_params.distros
+
 
 updates_generator = conf.UpdatesFile(conf_params.distros,
                                      conf_params.architectures)
 
 dist = conf_params.create_distributions_file(updates_generator)
 
-for ubuntu_distro in options.distro:
+for ubuntu_distro in distros:
+    for arch in arches:
 
-    d = {'name': 'ros-%s-%s-%s' %
-         (options.rosdistro, ubuntu_distro, options.arch),
-         'method': options.upstream_ros,
-         'suites': ubuntu_distro,
-         'component': 'main',
-         'architectures': options.arch,
-         'filter_formula': 'Package (%% ros-%s-*)' % options.rosdistro,
-         }
+        d = {'name': 'ros-%s-%s-%s' %
+             (options.rosdistro, ubuntu_distro, arch),
+             'method': options.upstream_ros,
+             'suites': ubuntu_distro,
+             'component': 'main',
+             'architectures': arch,
+             'filter_formula': 'Package (%% ros-%s-*)' % options.rosdistro,
+             }
 
-    updates_generator.add_update_element(conf.UpdateElement(**d))
+        updates_generator.add_update_element(conf.UpdateElement(**d))
 
 # clean up first
 if not options.no_cleanup:
-    for distro in options.distro:
-        run_cleanup(conf_params.repository_path, options.rosdistro, distro,
-                    options.arch, options.commit)
-for distro in options.distro:
-    run_update(conf_params.repository_path, dist, updates_generator,
-               distro, options.arch, options.commit)
+    for distro in distros:
+        for arch in arches:
+            run_cleanup(conf_params.repository_path, options.rosdistro, distro,
+                        arch, options.commit)
+for distro in distros:
+    for arch in arches:
+        run_update(conf_params.repository_path, dist, updates_generator,
+                   distro, arch, options.commit)
