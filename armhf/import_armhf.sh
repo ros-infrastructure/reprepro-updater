5942bbff550b9511e3a9bd34bc5423975f1ca406
Tully Foote <tfoote@osrfoundation.org>
2014-12-24 13:36:51 -0800

    updating readme removing old script

--- armhf/import_armhf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=/home/rosbuild/reprepro_updater/src
-
-
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros-shadow-fixed/ubuntu/ -r indigo -d trusty -a armhf -u http://packages.namniart.com/repos/ros -c
