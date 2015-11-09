0869c882de0935c61aaf643d4b513a20734df23d
Tully Foote <tfoote@osrfoundation.org>
2015-11-08 23:30:28 -0800

    adding a basic unit test

---
+++ test/test_diff.py
@@ -0,0 +1,27 @@
+import os
+from reprepro_updater import diff_repos
+
+
+def test_announcement():
+    try:
+        target_url = 'file://' + os.path.abspath(os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            'test_data/Packages.target'))
+        pf_old = diff_repos.get_packagefile_from_url(target_url, name='old')
+    except RuntimeError as ex:
+        assert False
+
+    try:
+        upstream_url = 'file://' + os.path.abspath(os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            'test_data/Packages.upstream'))
+        pf_new = diff_repos.get_packagefile_from_url(upstream_url, name='new')
+    except RuntimeError as ex:
+        assert False
+
+    announcement = diff_repos.compute_annoucement('indigo', pf_old, pf_new)
+
+    ## Spot check results
+    assert 'Removed Packages [10]:' in announcement
+    assert 'Updated Packages [1740]:' in announcement
+    assert 'Added Packages [214]:' in announcement
