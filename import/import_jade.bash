c0725a06bd1587be9b5dd337d86b9a0250ca775f
Tully Foote <tfoote@willowgarage.com>
2016-01-19 04:08:55 -0500

    adding importers from repositories.ros.org

---
+++ import/import_jade.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export PYTHONPATH=/home/rosbuild/reprepro_updater/src
+
+
+set -o errexit
+
+## Import jade from production buildfarm
+
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/import/repositories.ros.org_jade_testing.yaml
+
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -y /home/rosbuild/reprepro_updater/import/repositories.ros.org_jade_main.yaml
+
