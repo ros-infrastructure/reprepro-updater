951812014a2f9afef53ffd90fa06a41af3fd8b4f
Tully Foote <tfoote@willowgarage.com>
2016-02-05 20:54:38 -0500

    add xenial for kinetic support

--- src/reprepro_updater/conf.py
+++ src/reprepro_updater/conf.py
@@ -34,7 +34,7 @@ import os
 
 ALL_DISTROS = ['hardy', 'jaunty', 'karmic', 'lucid', 'maverick',
                'natty', 'oneiric', 'precise', 'quantal', 'raring',
-               'saucy', 'trusty', 'utopic', 'vivid', 'wily',
+               'saucy', 'trusty', 'utopic', 'vivid', 'wily', 'xenial',
                'wheezy', 'jessie']
 ALL_ARCHES =  ['amd64', 'i386', 'arm64', 'armel', 'armhf', 'source']
 
