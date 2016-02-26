a03fffb39a51e7fc25962ff7943144eed4040cd9
Tully Foote <tfoote@willowgarage.com>
2016-02-26 03:26:48 -0500

    improve documentation and update import rules

--- import/import_indigo.bash
+++ import/import_indigo.bash
@@ -7,7 +7,7 @@ set -o errexit
 
 ## Import indigo armhf from experimental farm
 
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/import/repositories.ros.org_indigo_testing.yaml
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/import/repositories.ros.org_indigo_testing.yaml --commit
 
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -y /home/rosbuild/reprepro_updater/import/repositories.ros.org_indigo_main.yaml
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -y /home/rosbuild/reprepro_updater/import/repositories.ros.org_indigo_main.yaml --commit
 
--- import/import_jade.bash
+++ import/import_jade.bash
@@ -7,7 +7,7 @@ set -o errexit
 
 ## Import jade from production buildfarm
 
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/import/repositories.ros.org_jade_testing.yaml
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros-shadow-fixed/ubuntu -y /home/rosbuild/reprepro_updater/import/repositories.ros.org_jade_testing.yaml --commit
 
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -y /home/rosbuild/reprepro_updater/import/repositories.ros.org_jade_main.yaml
+python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/www/repos/ros/ubuntu -y /home/rosbuild/reprepro_updater/import/repositories.ros.org_jade_main.yaml --commit
 
