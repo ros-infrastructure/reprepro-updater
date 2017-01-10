179380e808df340e6689bad7b4aee75db6599770
Tully Foote <tfoote@willowgarage.com>
2017-01-10 02:20:08 -0500

    the only repo valid on this server now is ros_bootstrap

--- scripts/update_repos.sh
+++ scripts/update_repos.sh
@@ -4,6 +4,4 @@
 # This is also useful when adding support for a new Ubuntu distro. 
 
 export PYTHONPATH=/home/rosbuild/reprepro_updater/src
-python /home/rosbuild/reprepro_updater/scripts/setup_repo.py /var/www/repos/building -c
-python /home/rosbuild/reprepro_updater/scripts/setup_repo.py /var/www/repos/ros-shadow-fixed/ubuntu -c
-python /home/rosbuild/reprepro_updater/scripts/setup_repo.py /var/www/repos/ros/ubuntu -c
+python /home/rosbuild/reprepro_updater/scripts/setup_repo.py /var/www/repos/ros_bootstrap -c
