04eb17313ed769d8820ae0168a55ecec7b8de65b
Mikael Arguedas <mikael@osrfoundation.org>
2017-06-05 16:26:57 -0700

    add artful

--- src/reprepro_updater/conf.py
+++ src/reprepro_updater/conf.py
@@ -35,7 +35,7 @@ import os
 ALL_DISTROS = ['hardy', 'jaunty', 'karmic', 'lucid', 'maverick',
                'natty', 'oneiric', 'precise', 'quantal', 'raring',
                'saucy', 'trusty', 'utopic', 'vivid', 'wily', 'xenial',
-               'yakkety', 'zesty',
+               'yakkety', 'zesty', 'artful',
                'wheezy', 'jessie', 'stretch']
 ALL_ARCHES =  ['amd64', 'i386', 'arm64', 'armel', 'armhf', 'source']
 
