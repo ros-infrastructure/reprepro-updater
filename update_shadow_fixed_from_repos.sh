8f425c6ecaac4f0575b128458b7be162c20ddeb1
Tully Foote <tfoote@osrfoundation.org>
2018-01-02 14:44:58 -0800

    remove outdated scripts (#52)
    * remove outdated scripts
    
    * removing migration scripts too

--- update_shadow_fixed_from_repos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=/home/rosbuild/reprepro_updater/src
-
-
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros-shadow-fixed/ubuntu -r hydro -d precise -d quantal -d raring -a i386 -u http://repos.ros.org/repos/ros-shadow-fixed/ubuntu -c
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros-shadow-fixed/ubuntu -r hydro -d precise -d quantal -d raring -a amd64 -u http://repos.ros.org/repos/ros-shadow-fixed/ubuntu -c 
-python /home/rosbuild/reprepro_updater/scripts/prepare_sync.py /var/packages/ros-shadow-fixed/ubuntu -r hydro -d precise -d quantal -d raring -a source -u http://repos.ros.org/repos/ros-shadow-fixed/ubuntu -c
