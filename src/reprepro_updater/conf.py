b257851cf8d0d28d6e633d22373411c0f39ac89f
Tully Foote <tfoote@willowgarage.com>
2018-01-22 13:42:41 -0500

    adding buster to the list of supported distros

--- src/reprepro_updater/conf.py
+++ src/reprepro_updater/conf.py
@@ -36,7 +36,7 @@ ALL_DISTROS = ['hardy', 'jaunty', 'karmic', 'lucid', 'maverick',
                'natty', 'oneiric', 'precise', 'quantal', 'raring',
                'saucy', 'trusty', 'utopic', 'vivid', 'wily', 'xenial',
                'yakkety', 'zesty', 'artful', 'bionic',
-               'wheezy', 'jessie', 'stretch']
+               'wheezy', 'jessie', 'stretch', 'buster']
 ALL_ARCHES =  ['amd64', 'i386', 'arm64', 'armel', 'armhf', 'source']
 
 class ConfParameters(object):
