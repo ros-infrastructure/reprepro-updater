1ec8f67a443f80406d88237a8899eb46371f788c
Shane Loretz <sloretz@osrfoundation.org>
2020-01-03 10:57:16 -0800

    Add focal to list of distros
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- src/reprepro_updater/conf.py
+++ src/reprepro_updater/conf.py
@@ -35,7 +35,7 @@ import os
 ALL_DISTROS = ['hardy', 'jaunty', 'karmic', 'lucid', 'maverick',
                'natty', 'oneiric', 'precise', 'quantal', 'raring',
                'saucy', 'trusty', 'utopic', 'vivid', 'wily', 'xenial',
-               'yakkety', 'zesty', 'artful', 'bionic',
+               'yakkety', 'zesty', 'artful', 'bionic', 'focal',
                'wheezy', 'jessie', 'stretch', 'buster']
 ALL_ARCHES =  ['amd64', 'i386', 'arm64', 'armel', 'armhf', 'source']
 
