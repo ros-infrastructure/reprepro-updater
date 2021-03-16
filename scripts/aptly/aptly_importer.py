111d049e9b75ff8ebe8a345f7e4d2788fa8c9565
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-03-16 17:18:15 +0100

    Implement the filter transformation

--- scripts/aptly/aptly_importer.py
+++ scripts/aptly/aptly_importer.py
@@ -6,18 +6,30 @@ from sys import stderr
 import yaml
 
 
+class Reprepro2AptlyFilter():
+    def convert(self, filter_str):
+        # remove begin/end whitespaces
+        r_str = filter_str.lstrip()
+        # Package menas nothing as filter. Could be Name or empty.
+        r_str = r_str.replace('Package', 'Name')
+        # Do not use equal symbols, just remove
+        r_str = r_str.replace('% =', '')
+        return r_str
+
+
 class Aptly():
-    def run(self, cmd=[]):
+    def run(self, cmd=[], fail_on_errors=True, show_errors=True):
         run_cmd = ['aptly'] + cmd
+        # print(f"RUN {' '.join(run_cmd)}")
         try:
             r = run(run_cmd, stdout=PIPE, stderr=PIPE)
         except CalledProcessError as e:
-            print(f"Aptly error: {e.stderr.decode('utf-8')} \n", file=stderr)
             return False
         if r.returncode == 0:
             return True
         else:
-            print(f"Aptly error: {r.stderr.decode('utf-8')} \n", file=stderr)
+            if show_errors:
+                self.error(run_cmd, f"{r.stderr.decode('utf-8')}", fail_on_errors)
             return False
 
     def check_valid_filter(self, filter_str):
@@ -26,23 +38,37 @@ class Aptly():
                              f"-filter={filter_str}",
                              fake_mirror_name,
                              'http://deb.debian.org/debian', 'sid', 'main']
-        result = self.run(create_mirror_cmd)
+        result = self.run(create_mirror_cmd, fail_on_errors=False)
         if not result:
             return result
         delete_mirror_cmd = ['mirror', 'drop', fake_mirror_name]
         self.run(delete_mirror_cmd)
-
         return True
+
+    def check_mirror_exists(self, mirror_name):
+        return self.run(['mirror', 'show', mirror_name],
+                        fail_on_errors=False, show_errors=False)
+
+    def error(self, cmd, msg, exit=False):
+        print(f"Aptly error running: {cmd} \n", file=stderr)
+        print(f"{msg} \n", file=stderr)
 
 
 class UpdaterConfiguration():
     def __init__(self, input_file):
         try:
             self.config = self.load_config_file(input_file)
+            self.reprepro2aptly = Reprepro2AptlyFilter()
 
             self.architectures = self.config['architectures']
+            # source was accepted as a valid architecture to indicate that
+            # source packages need to be download. It is not a valid arch in aptly
+            if 'source' in self.config['architectures']:
+                self.architectures = self.config['architectures'].remove('source')
+            self.architectures = self.config['architectures']
             self.component = self.config['component']
-            self.filter_formula = self.config['filter_formula']
+            self.filter_formula = self.reprepro2aptly.convert(
+                self.config['filter_formula'])
             self.method = self.config['method']
             self.name = self.config['name']
             self.suites = self.config['suites']
@@ -65,6 +91,45 @@ class UpdaterConfiguration():
         exit(-1)
 
 
+class UpdaterManager():
+    def __init__(self, input_file):
+        self.config = UpdaterConfiguration(input_file)
+        self.aptly = Aptly()
+
+    def get_mirror_name(self, distribution):
+        return f"{self.config.name}-{distribution}"
+
+    def create_aptly_mirrors_from_config(self):
+        for dist in self.config.suites:
+            self.create_aptly_mirror(dist)
+
+    def create_aptly_mirror(self, distribution):
+        assert(self.config)
+        self.aptly.run(['mirror', 'create', '-with-sources',
+                        f"-architectures={','.join(self.config.architectures)}",
+                        f"-filter={self.config.filter_formula}",
+                        self.get_mirror_name(distribution),
+                        self.config.method,
+                        distribution,
+                        self.config.component])
+
+    def check_aptly_mirrors_exist(self):
+        for dist in self.config.suites:
+            mirror_name = self.get_mirror_name(dist)
+            if self.aptly.check_mirror_exists(mirror_name):
+                self.error(f"mirror {mirror_name} exists. Refuse to create mirrors")
+
+    def run(self):
+        # check that mirror names are available
+        self.check_aptly_mirrors_exist()
+        for dist in self.config.suites:
+            self.create_aptly_mirror(dist)
+
+    def error(self, msg):
+        print(f"Update Manager error: {msg} \n", file=stderr)
+        exit(-1)
+
+
 def main():
     """
     Usage: python3 aptly_importer.py <config_file>
@@ -79,7 +144,11 @@ def main():
     if not path.exists(args.config_file[0]):
         parser.error("Missing input file from %s" % args.config_file[0])
 
-    config = UpdaterConfiguration(args.config_file[0])
+    # 1. Create aptly mirrors from yaml configuration file
+    manager = UpdaterManager(args.config_file[0])
+    # check aptly mirrors before creating to avoid problems beforehand
+    manager.check_aptly_mirrors_exist()
+    manager.create_aptly_mirrors_from_config()
 
 
 if __name__ == '__main__':
--- scripts/aptly/aptly_importer_TEST.py
+++ scripts/aptly/aptly_importer_TEST.py
@@ -5,7 +5,9 @@ import aptly_importer
 #class TestConfigBase(unittest.TestCase):
 #    def setUp(self):
 #        self.config = repository.load_config_file('config/_test_repository.yaml')
-
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 871920D1991BC93C
+#http://packages.osrfoundation.org/gazebo/ubuntu
 
 class TestYamlConfiguration(unittest.TestCase):
     def test_non_existing_file(self):
@@ -32,6 +34,50 @@ class TestAptly(unittest.TestCase):
 
     def test_invalid_filter(self):
         self.assertFalse(self.aptly.check_valid_filter('mysql-client (>= 3.6'))
+
+    def tets_invalid_mirror(self):
+        self.assertFalse(self.aptly.check_mirror_exists('i_do_not_exist'))
+
+
+class TestUpdaterManager(unittest.TestCase):
+    def setUp(self):
+        self.aptly = aptly_importer.Aptly()
+        self.expected_mirror_test_name = '_reprepro_updater_test_suite_-focal'
+
+    def _remove_mirror(self, mirror_name):
+        self.aptly.run(['mirror', 'drop', mirror_name], show_errors=False, fail_on_errors=False)
+
+    def test_example_creation(self):
+        self._remove_mirror(self.expected_mirror_test_name)
+        manager = aptly_importer.UpdaterManager('test/example.yaml')
+        manager.run()
+        self.assertTrue(self.aptly.check_mirror_exists(self.expected_mirror_test_name))
+        self._remove_mirror(self.expected_mirror_test_name)
+
+
+class TestReprepro2AptlyFilter(unittest.TestCase):
+    def setUp(self):
+        self.aptly = aptly_importer.Aptly()
+        self.filter = aptly_importer.Reprepro2AptlyFilter()
+
+    def _assert_valid_filter(self, filter_str):
+        self.assertTrue(self.aptly.check_valid_filter(
+            self.filter.convert(filter_str)))
+
+    def test_package(self):
+        self._assert_valid_filter('Package (% sdformat)')
+
+    def test_package_version(self):
+        self._assert_valid_filter('Package (% sdformat*), $Version (>= 6.2.0+dfsg-2build1)')
+
+    def test_multi_package_version(self):
+        filter_formula = """ \
+        ((Package (% =lark-parser) |\
+          Package (% =python3-lark-parser) |\
+          Package (% =python3-lark-parser-doc)),
+          $Version (% 0.7.2-3osrf~* ))\
+        """
+        self._assert_valid_filter(filter_formula)
 
 
 if __name__ == '__main__':
